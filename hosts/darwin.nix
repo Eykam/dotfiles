3e62c0ad3c908ec5f35895ad3a1594012cb20ec3
Eyad Kamil <ekamil272@gmail.com>
2026-02-16 15:58:48 -0500

    remove dir creation in script

--- hosts/darwin.nix
+++ hosts/darwin.nix
@@ -8,19 +8,6 @@ in
     raycast
   ];
 
-  # Symlink nix .app bundles to ~/Applications so Raycast/Spotlight can find them
-  home.activation.aliasApplications = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    app_dir="${homeDirectory}/Applications/Home Manager Apps"
-    mkdir -p "$app_dir"
-    for app in "$genProfilePath/home-path/Applications/"*.app; do
-      [ -d "$app" ] || continue
-      app_name="$(basename "$app")"
-      target="$app_dir/$app_name"
-      rm -f "$target"
-      /usr/bin/osascript -e "tell application \"Finder\" to make alias file to POSIX file \"$app\" at POSIX file \"$app_dir\"" || ln -sf "$app" "$target"
-    done
-  '';
-
   # VS Code settings (macOS path)
   home.file."Library/Application Support/Code/User/settings.json" = {
     source = config.lib.file.mkOutOfStoreSymlink "${hmDir}/configs/vscode/settings.json";
